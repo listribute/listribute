098917a3b000769e6462ecb56dd5dc3a83f195e9
Vegard Løkken <vegard@loekken.org>
2020-02-11 18:18:43 +0100

    Create and login with stored user

--- Listribute/App.tsx
+++ Listribute/App.tsx
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
-import { login } from "./app/api";
+import * as api from "./app/api";
 import HomePage from "./app/components/HomePage";
 import ListPage from "./app/components/ListPage";
 import { List } from "./app/model/list";
 import { User } from "./app/model/user";
+import * as storage from "./app/storage";
 
 const App: React.FC = () => {
-    const [user, setUser] = useState<User>();
+    const [currentUser, setCurrentUser] = useState<User>();
     const [currentList, setCurrentList] = useState<List>();
 
     useEffect(() => {
         (async () => {
-            // TODO: Get credentials from storage
-            const user = await login({
-                username: "listribute_1536",
-                password: "68793008a978",
-            });
-            setUser(user);
+            // Look for user credentials in storage
+            let user = await storage.getUser();
+            if (!user) {
+                // Create a new user automatically
+                user = await api.createNewUser();
+                // Store the new user's credentials
+                storage.setUser(user);
+            }
+            // Login to API to set session cookie for future requests
+            user = await api.login(user);
+            // Update state with new user
+            setCurrentUser(user);
         })();
     }, []);
 
-    return user === undefined ? null : (
+    return currentUser === undefined ? null : (
         <SafeAreaView>
             {currentList ? (
                 <ListPage
-                    username={user.username}
+                    username={currentUser.username}
                     list={currentList}
                     onBack={() => setCurrentList(undefined)}
                 />
             ) : (
                 <HomePage
-                    user={user}
+                    user={currentUser}
                     onSelectList={list => setCurrentList(list)}
                 />
             )}
