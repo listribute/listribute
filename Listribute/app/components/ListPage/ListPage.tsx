listribute/listribute

commit 789369bde4b884d8172f34ccf9c51f0077dda7a5

Move derived state into list class
So it can be used from more places.

Listribute/app/components/ListPage/ListPage.tsx
@@ -236,15 +236,9 @@ const ListPage: React.FC<Props> = ({ navigation, route }) => {
         });
     }, [navigation, inputFocused, addButton, menu]);
 
-    const isOwnWishList =
-        !!list?.wishList && list.createdBy === state.currentUser.id;
-
-    const isOthersWishList =
-        !!list?.wishList && list.createdBy !== state.currentUser.id;
-
     return (
         <View style={styles.container}>
-            {!isOthersWishList && (
+            {!list?.isOthersWishList && (
                 <Input
                     ref={inputRef}
                     placeholder="New item"
@@ -267,7 +261,9 @@ const ListPage: React.FC<Props> = ({ navigation, route }) => {
                     renderItem={({ item }) => (
                         <ListItem
                             item={item}
-                            isOwnWishList={isOwnWishList}
+                            isOwnWishList={
+                                !!list?.isOwnWishList(state.currentUser.id)
+                            }
                             checkItem={checkItem}
                             onPress={() => goToItem(item)}
                         />

Listribute/app/model/list.ts
@@ -20,4 +20,12 @@ export class List {
     addSubscriber(username: string) {
         this._subscribers = [...this.subscribers, username];
     }
+
+    isOwnWishList(userId: number) {
+        return !!this?.wishList && this.createdBy === userId;
+    }
+
+    isOthersWishList(userId: number) {
+        return !!this?.wishList && this.createdBy !== userId;
+    }
 }

commit 65470e24b9bdf209a849bad2c3c0c77adb6cfd5d

Fix missing input field in list page
Fixing regression introduced in commit 789369bd.

Listribute/app/components/ListPage/ListPage.tsx
@@ -238,7 +238,7 @@ const ListPage: React.FC<Props> = ({ navigation, route }) => {
 
     return (
         <View style={styles.container}>
-            {!list?.isOthersWishList && (
+            {!list?.isOthersWishList(state.currentUser.id) && (
                 <Input
                     ref={inputRef}
                     placeholder="New item"